2ddaf79bd7e308fa74e7b186e49ff8e902463a33
Radu Cretu <ameriza@gmail.com>
2017-07-23 14:09:39 +0300

    v0.0.1

---
+++ includes/Actions/DripPostSubscriber.php
@@ -0,0 +1,215 @@
+<?php if ( ! defined( 'ABSPATH' ) || ! class_exists( 'NF_Abstracts_Action' )) exit;
+
+/**
+ * Class NF_Drip_Actions_DripPostSubscriber
+ */
+final class NF_Drip_Actions_DripPostSubscriber extends NF_Abstracts_Action
+{
+    /**
+     * @var string
+     */
+    protected $_name  = 'drip';
+
+    /**
+     * @var array
+     */
+    protected $_tags = array();
+
+    /**
+     * @var string
+     */
+    protected $_timing = 'normal';
+
+    /**
+     * @var int
+     */
+    protected $_priority = '10';
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+{
+    parent::__construct();
+
+    $this->_nicename = __( 'POST Drip Subscriber', 'ninja-forms-drip' );
+
+    /*
+     * Settings
+     */
+    $this->_settings = array(
+      'drip_subscriber' => array(
+        'name' => 'drip_subscriber',
+        'type' => 'textbox',
+        'label' => __( 'Subscriber Email', 'ninja-forms-drip'),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => '',
+        'placeholder' => __( 'Choose Drip subscriber email', 'ninja-forms-drip' ),
+        'help' => __( "Mandatory. The subscriber's email address.", 'ninja-forms-drip' ),
+        'use_merge_tags' => true
+      ),
+      'drip_custom_fields' => array(
+        'name' => 'drip_custom_fields',
+        'type' => 'textarea',
+        'label' => __( 'Custom Fields', 'ninja-forms-drip'),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => '',
+        'placeholder' => __( 'Choose custom fields.', 'ninja-forms-drip' ),
+        'help' => __( 'Optional. An Object containing custom field data. E.g. { name: john doe }.', 'ninja-forms-drip' ),
+        'use_merge_tags' => true
+      ),
+      'drip_tags' => array(
+        'name' => 'drip_tags',
+        'type' => 'textbox',
+        'label' => __( 'Add Tags', 'ninja-forms-drip'),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => '',
+        'placeholder' => __( 'Comma separated list (ex: new, promotion ...)', 'ninja-forms-drip' ),
+        'help' => __( 'Optional. A comma separated list containing one or more tags. E.g. (ex: new, promotion ...).', 'ninja-forms-drip' ),
+        'use_merge_tags' => true
+      ),
+      'drip_remove_tags' => array(
+        'name' => 'drip_tags_remove',
+        'type' => 'textbox',
+        'label' => __( 'Remove Tags', 'ninja-forms-drip'),
+        'width' => 'full',
+        'group' => 'primary',
+        'value' => '',
+        'placeholder' => __( 'Comma separated list (ex: new, promotion ...)', 'ninja-forms-drip' ),
+        'help' => __( 'Optional. A comma separated list containing one or more tags. E.g. (ex: new, promotion ...).', 'ninja-forms-drip' ),
+        'use_merge_tags' => true
+      ),
+      'drip_prospect' => array(
+        'name' => 'drip_prospect',
+        'type' => 'toggle',
+        'label' => __( 'Prospect', 'ninja-forms-drip'),
+        'width' => 'one-third',
+        'group' => 'advanced',
+        'value' => true,
+        'help' => __( 'Optional. Specifiy whether we should attach a lead score to the subscriber (when lead scoring is enabled).', 'ninja-forms-drip' ),
+        'use_merge_tags' => true
+      ),
+      'lifetime_value' => array(
+        'name' => 'drip_lifetime_value',
+        'type' => 'textbox',
+        'label' => __( 'Lifetime Value', 'ninja-forms-drip'),
+        'width' => 'one-third',
+        'group' => 'advanced',
+        'value' => '',
+        'placeholder' => __( 'Enter value in cents', 'ninja-forms-drip' ),
+        'help' => __( 'Optional. The lifetime value of the subscriber (in cents).', 'ninja-forms-drip' ),
+        'use_merge_tags' => true
+      ),
+      'drip_base_lead_score' => array(
+        'name' => 'drip_base_lead_score',
+        'type' => 'textbox',
+        'label' => __( 'Base Lead Score', 'ninja-forms-drip'),
+        'width' => 'one-third',
+        'group' => 'advanced',
+        'value' => 30,
+        'help' => __( 'Optional. An Integer specifying the starting value for lead score calculation for this subscriber. Defaults to 30.', 'ninja-forms-drip' ),
+        'use_merge_tags' => true,
+      ),
+    );
+}
+
+    /*
+    * PUBLIC METHODS
+    */
+
+    public function save( $action_settings )
+    {
+
+    }
+
+    public function process( $action_settings, $form_id, $data )
+    {
+
+      if( ! is_email( $action_settings['drip_subscriber'] ) ) return $data;
+
+      $tags = $remove_tags = $custom_fields = array();
+
+      $user_tags = explode( ',', $action_settings['drip_tags'] );
+
+      if( ! empty( $user_tags ) && is_array( $user_tags ) ){
+        foreach( $user_tags as $key => $tag ){
+          $tag = trim( $tag );
+          if( ! empty( $tag ) )
+          $tags[] = sanitize_text_field( $tag );
+        }
+      }
+
+      $user_remove_tags = explode( ',', $action_settings['drip_remove_tags'] );
+
+      if( ! empty( $user_remove_tags ) && is_array( $user_remove_tags ) ){
+        foreach( $user_remove_tags as $key => $tag ){
+          $tag = trim( $tag );
+          if( ! empty( $tag ) )
+          $remove_tags[] = sanitize_text_field($tag);
+        }
+      }
+
+      $user_custom_fields = explode("\n", $action_settings['drip_custom_fields']);
+
+      if( ! empty( $user_custom_fields ) && is_array( $user_custom_fields ) ){
+        foreach( $user_custom_fields as $key => $pair ){
+          $pair = trim( $pair );
+          if( ! empty( $pair ) ){
+            $field = explode( ':', $pair );
+            if( ! empty( $field ) && is_array( $field ) ){
+              if( isset( $field[0] ) && isset( $field[1] ) && ! empty( $field[0] ) && ! empty( $field[1] ) ){
+                $custom_fields[ sanitize_key( trim($field[0]) ) ] = sanitize_text_field( trim( $field[1] ) );
+              }
+            }
+          }
+        }
+      }
+
+      $subscriber = array( 'email' => $action_settings['drip_subscriber'] );
+
+      if( ! empty( $custom_fields ) ){
+        $subscriber['custom_fields'] = $custom_fields;
+      }
+
+      if( ! empty( $tags ) ){
+        $subscriber['tags'] = $tags;
+      }
+
+      if( ! empty( $remove_tags ) ){
+        $subscriber['remove_tags'] = $remove_tags;
+      }
+
+      if( intval( $action_settings['drip_lifetime_value'] ) > 0 ){
+        $subscriber['lifetime_value'] = intval( $action_settings['drip_lifetime_value'] );
+      }
+
+      if( ! wp_validate_boolean( $action_settings['drip_prospect'] ) ){
+        $subscriber['prospect'] = wp_validate_boolean( $action_settings['drip_prospect'] );
+      }
+
+      if( intval( $action_settings['drip_base_lead_score'] ) !== 30 ){
+        $subscriber['base_lead_score'] = intval( $action_settings['drip_base_lead_score'] );
+      }
+
+      error_log(print_r($subscriber, true));
+
+      $response  = NF_Drip()->remote_post(
+        "http://api.getdrip.com/v2/{:account_id}/subscribers", array( 'subscribers' => array( $subscriber )
+      ));
+
+      if( ! is_wp_error( $response ) ){
+        $response = wp_remote_retrieve_body( $response );
+        $response = json_decode($response);
+        $response = isset( $response->subscribers ) ? $response->subscribers[0] : false;
+      }
+
+      if( $response !== false ){
+        do_action( 'ninja-forms-drip-response', $response, $action_settings, $form_id, $data );
+      }
+
+      return $data;
+    }
+}
---
+++ ninja-forms-drip.php
@@ -0,0 +1,405 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/*
+ * Plugin Name: Ninja Forms - Drip
+ * Plugin URI:
+ * Description:
+ * Version: 3.0.0
+ * Author:
+ * Author URI:
+ * Text Domain: ninja-forms-drip
+ *
+ * Copyright 2017 .
+ */
+
+if( version_compare( get_option( 'ninja_forms_version', '0.0.0' ), '3', '<' ) || get_option( 'ninja_forms_load_deprecated', FALSE ) ) {
+
+    //include 'deprecated/ninja-forms-drip.php';
+
+} else {
+
+    /**
+     * Class NF_Drip
+     */
+    final class NF_Drip
+    {
+        const VERSION = '0.0.1';
+        const SLUG    = 'drip';
+        const NAME    = 'Drip';
+        const AUTHOR  = '';
+        const PREFIX  = 'NF_Drip';
+
+        /**
+         * @var NF_Drip
+         * @since 3.0
+         */
+        private static $instance;
+
+        /**
+         * Plugin Directory
+         *
+         * @since 3.0
+         * @var string $dir
+         */
+        public static $dir = '';
+
+        /**
+         * Plugin URL
+         *
+         * @since 3.0
+         * @var string $url
+         */
+        public static $url = '';
+
+        /**
+         * Main Plugin Instance
+         *
+         * Insures that only one instance of a plugin class exists in memory at any one
+         * time. Also prevents needing to define globals all over the place.
+         *
+         * @since 3.0
+         * @static
+         * @static var array $instance
+         * @return NF_Drip Highlander Instance
+         */
+        public static function instance()
+        {
+            if (!isset(self::$instance) && !(self::$instance instanceof NF_Drip)) {
+                self::$instance = new NF_Drip();
+
+                self::$dir = plugin_dir_path(__FILE__);
+
+                self::$url = plugin_dir_url(__FILE__);
+
+                /*
+                 * Register our autoloader
+                 */
+                spl_autoload_register(array(self::$instance, 'autoloader'));
+            }
+
+            return self::$instance;
+        }
+
+        public function __construct()
+        {
+            /*
+             * Required for all Extensions.
+             */
+            add_action( 'admin_init', array( $this, 'setup_license') );
+
+            add_filter( 'ninja_forms_register_actions', array($this, 'register_actions'));
+
+            add_filter( 'ninja_forms_plugin_settings', array( $this, 'plugin_settings' ), 10, 1 );
+            add_filter( 'ninja_forms_plugin_settings_groups', array( $this, 'plugin_settings_groups' ), 10, 1 );
+
+            add_action( 'init', array( $this, 'custom_rewrite_basic' ) );
+            add_filter( 'query_vars', array( $this, 'add_query_vars' ) );
+            add_filter( 'parse_request', array( $this, 'parse_request' ), 100 );
+
+            add_action( 'ninja_forms_save_setting_drip_secret',   array( $this, 'save_drip_secret' ), 10, 1 );
+
+            add_action( 'admin_post_ninja_forms_drip_authorize', array( $this, 'authorize_drip' ) );
+            add_action( 'admin_post_ninja_forms_drip_deauthorize', array( $this, 'deauthorize_drip' ) );
+
+        }
+
+        public static function has_token()
+        {
+          $token = Ninja_Forms()->get_setting('drip_token');
+          return ! $token || empty( $token ) ? false : true;
+        }
+
+        public static function remote_post( $url, $body )
+        {
+
+          $patterns = array();
+      		$patterns[0] = "/{:account_id}/";
+      		$replacements = array();
+      		$replacements[0] = self::get_drip_account();
+      		$url = preg_replace( $patterns, $replacements, $url );
+
+          return wp_remote_post( $url, array(
+            'body' => json_encode( $body ),
+            'headers' => array(
+              'Content-Type' => 'application/json',
+              'Authorization' => 'Bearer ' . Ninja_Forms()->get_setting('drip_token')
+              )
+            )
+          );
+        }
+
+        public static function remote_get( $url )
+        {
+          return wp_remote_get( $url, array(
+            'headers' => array(
+              'Authorization' => 'Bearer ' . Ninja_Forms()->get_setting('drip_token')
+              )
+            )
+          );
+        }
+
+        function custom_rewrite_basic()
+        {
+          add_rewrite_rule('^ninja-forms-drip-authorize/', 'index.php?ninja_forms_drip_authorize=true', 'top');
+        }
+
+        function parse_request()
+        {
+          global $wp_query;
+          if( ! is_user_logged_in() )
+          if( ! isset( $wp_query->query['ninja-forms-drip-authorize'] ) ) return;
+          if( isset( $_GET['code'] ) ) do_action( 'admin_post_ninja_forms_drip_authorize' );
+        }
+
+        function add_query_vars($vars){
+          $vars[] = "ninja-forms-drip-authorize";
+          return $vars;
+        }
+
+        public static function get_drip_account( $email = false, $url = false )
+        {
+
+          $drip_account = Ninja_Forms()->get_setting('drip_account');
+          $drip_account_json = Ninja_Forms()->get_setting('drip_account_json');
+
+          if( empty( $drip_account ) ){
+            $response = self::remote_get( 'http://api.getdrip.com/v2/accounts/' );
+
+            if ( ! is_wp_error( $response ) ) {
+              $accounts = json_decode( wp_remote_retrieve_body( $response ) );
+              if( $accounts !== false && ! empty( $accounts ) && isset( $accounts->accounts )  && is_array( $accounts->accounts ) ){
+                $drip_account = $accounts->accounts[0]->id;
+                $drip_account_json = $accounts->accounts[0];
+                Ninja_Forms()->update_setting('drip_account_json', $accounts->accounts[0]);
+                Ninja_Forms()->update_setting('drip_account', $drip_account);
+              }
+            }
+          }
+
+          if( $email ){
+            $drip_account = $drip_account_json->primary_email;
+          }
+
+          if( $url ){
+            $drip_account = $drip_account_json->url;
+          }
+
+          return $drip_account;
+
+        }
+
+        public function deauthorize_drip()
+        {
+          Ninja_Forms()->update_setting( 'drip_token', '' );
+          Ninja_Forms()->update_setting( 'drip_client_id', '' );
+          Ninja_Forms()->update_setting( 'drip_secret', '' );
+          
+          wp_safe_redirect( add_query_arg( array(
+            'page' => 'nf-settings'
+          ), admin_url( '/admin.php' ) ) );
+          exit;
+        }
+
+        public function authorize_drip(){
+
+          if( ! isset( $_GET['code'] ) || empty( $_GET['code'] ) ){
+            wp_safe_redirect( add_query_arg( array(
+              'page' => 'nf-settings'
+            ), admin_url( '/admin.php' ) ) );
+            exit;
+
+          }
+
+          if( in_array( $_GET['code'], array( 'error', 'error_reason', 'error_description' ) ) ){
+            wp_safe_redirect( add_query_arg( array(
+              'page' => 'nf-settings'
+            ), admin_url( '/admin.php' ) ) );
+            exit;
+
+          }
+
+          $args = array(
+            'client_id' => Ninja_Forms()->get_setting('drip_client_id'),
+            'client_secret' => Ninja_Forms()->get_setting('drip_secret'),
+            'grant_type' => 'authorization_code',
+            'response_type' => 'token',
+            'redirect_uri' => self::get_drip_redirect_url(),
+            'code' => esc_attr( $_GET['code'] )
+          );
+
+          $response = wp_remote_post( add_query_arg( $args, 'https://www.getdrip.com/oauth/token' ) );
+
+          $body = json_decode( wp_remote_retrieve_body( $response ) );
+
+          if( isset( $body->access_token ) && ! empty( $body->access_token ) ){
+            Ninja_Forms()->update_setting('drip_token', $body->access_token );
+          }
+
+          wp_safe_redirect( add_query_arg( array(
+            'page' => 'nf-settings',
+          ), admin_url( '/admin.php' ) ) );
+          exit;
+
+        }
+
+        public static function get_drip_redirect_url()
+        {
+          return esc_url_raw( site_url('/ninja-forms-drip-authorize/' ) );
+        }
+
+        public function save_drip_secret( $drip_secret )
+        {
+            $token = Ninja_Forms()->get_setting('drip_token');
+
+            if( empty( $token ) ){
+
+              $client_id = Ninja_Forms()->get_setting('drip_client_id');
+
+              if( ! empty( $client_id ) && ! empty( $drip_secret ) ){
+
+                wp_redirect( add_query_arg( array(
+                  'response_type' => 'code',
+                  'client_id' => $client_id,
+                  'redirect_uri' => urlencode( self::get_drip_redirect_url() )
+                ), 'https://www.getdrip.com/oauth/authorize' ) );
+              }
+
+            }
+
+        }
+
+
+        public function plugin_settings_groups( $groups )
+        {
+            $groups[ 'drip' ] = array(
+                'id' => 'drip',
+                'label' => __( 'Drip API Login', 'ninja-forms-drip' ),
+            );
+            return $groups;
+        }
+
+
+        public function plugin_settings( $settings )
+        {
+          $settings[ 'drip' ] = array();
+
+          if( $this->has_token() ){
+            $settings[ 'drip' ]['drip_token'] = array(
+                'id'    => 'drip_token',
+                'type'  => 'html',
+                'label'  => __( 'Drip API Status', 'ninja-forms-drip' ),
+                'html' => sprintf( '<p>%s <strong>%s</strong><br> %s %s</p><br>', __( 'Connected as', 'ninja-forms-drip' ), self::get_drip_account( true ), __( 'for account', 'ninja-forms-drip' ), self::get_drip_account( false, true ) ). '<a href="' . add_query_arg( array( 'action' => 'ninja_forms_drip_deauthorize' ), admin_url( '/admin-post.php' ) ) . '" class="button">' . __( 'Disconnect Drip Account', 'ninja-forms-drip' ) . '</a>',
+            );
+          }
+
+          else
+
+          {
+            $settings[ 'drip' ]['drip_redirect'] = array(
+                'id'    => 'drip_redirect',
+                'type'  => 'desc',
+                'label'  => __( 'Drip Callback URL', 'ninja-forms-drip' ),
+                'desc'  => self::get_drip_redirect_url(),
+            );
+            $settings[ 'drip' ]['drip_client_id'] = array(
+                'id'    => 'drip_client_id',
+                'type'  => 'textbox',
+                'label'  => __( 'Drip CLIENT ID', 'ninja-forms-drip' )
+            );
+            $settings[ 'drip' ]['drip_secret'] = array(
+                'id'    => 'drip_secret',
+                'type'  => 'textbox',
+                'label'  => __( 'Drip SECRET', 'ninja-forms-drip' )
+            );
+          }
+
+          return $settings;
+        }
+
+
+        /**
+         * Optional. If your extension processes or alters form submission data on a per form basis...
+         */
+        public function register_actions($actions)
+        {
+            if( $this->has_token() ){
+              $actions[ 'drip' ] = new NF_Drip_Actions_DripPostSubscriber();
+            }
+
+            return $actions;
+        }
+
+        /*
+         * Optional methods for convenience.
+         */
+
+        public function autoloader($class_name)
+        {
+            if (class_exists($class_name)) return;
+
+            if ( false === strpos( $class_name, self::PREFIX ) ) return;
+
+            $class_name = str_replace( self::PREFIX, '', $class_name );
+            $classes_dir = realpath(plugin_dir_path(__FILE__)) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR;
+            $class_file = str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+
+            if (file_exists($classes_dir . $class_file)) {
+                require_once $classes_dir . $class_file;
+            }
+        }
+
+        /**
+         * Template
+         *
+         * @param string $file_name
+         * @param array $data
+         */
+        public static function template( $file_name = '', array $data = array() )
+        {
+            if( ! $file_name ) return;
+
+            extract( $data );
+
+            include self::$dir . 'includes/Templates/' . $file_name;
+        }
+
+        /**
+         * Config
+         *
+         * @param $file_name
+         * @return mixed
+         */
+        public static function config( $file_name )
+        {
+            return include self::$dir . 'includes/Config/' . $file_name . '.php';
+        }
+
+        /*
+         * Required methods for all extension.
+         */
+
+        public function setup_license()
+        {
+        //  var_dump( get_option('ninja-drip-custom_fields') );
+            if ( ! class_exists( 'NF_Extension_Updater' ) ) return;
+            new NF_Extension_Updater( self::NAME, self::VERSION, self::AUTHOR, __FILE__, self::SLUG );
+        }
+    }
+
+    /**
+     * The main function responsible for returning The Highlander Plugin
+     * Instance to functions everywhere.
+     *
+     * Use this function like you would a global variable, except without needing
+     * to declare the global.
+     *
+     * @since 3.0
+     * @return {class} Highlander Instance
+     */
+    function NF_Drip()
+    {
+        return NF_Drip::instance();
+    }
+
+    NF_Drip();
+}
